36fac5fce121bb71006e6416ee7e2cac70a65ffa
Villalba <lmv@a483e7ad5050.ant.amazon.com>
2020-02-18 14:58:45 -0300

    fixing testing function

--- testingApp/myTestingFunction.js
+++ testingApp/myTestingFunction.js
@@ -3,24 +3,37 @@
 var AWS = require('aws-sdk');
 var codepipeline = new AWS.CodePipeline();
 
-
 // https://docs.aws.amazon.com/codepipeline/latest/userguide/actions-invoke-lambda-function.html
 exports.handler = async (event, context)  => {
 	console.log('Hello my testing function')
+	
 	// Retrieve the Job ID from the Lambda action
-	var jobId = event["CodePipeline.job"].id;
-	console.log(jobId);
+	let jobId = event["CodePipeline.job"].id;
 
-	var params = {
-		jobId: jobId
-	};
+	// DO SOME INTEGRATION TESTS
 
-	return codepipeline.putJobSuccessResult(params).promise(data => {
-		console.log('sent?')
-		console.log(data);
-		context.succeed('yey!');      
-	}).catch(error => {
-		console.log(error);
-		context.fail(error);
-	});
+	if (testsPassed) {
+		let params = {
+			jobId: jobId
+		};
+
+		return codepipeline.putJobSuccessResult(params).promise(data => {
+			context.succeed('Test passed');      
+		}).catch(error => {
+			context.fail(error);
+		});
+	} else {
+		let params = {
+			jobId: jobId,
+			failureDetails: {
+				message: JSON.stringify('Test failed'),
+				type: 'JobFailed',
+				externalExecutionId: context.invokeid
+			}
+		};
+		
+		return codepipeline.putJobFailureResult(params).promise(data => {
+			context.fail(message);      
+		});
+	}
 };
